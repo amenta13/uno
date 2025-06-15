3390575e0d131943b42b1b5cfea5db9bb02e8f41
amenta13 <amenta@etown.edu>
2025-06-15 10:47:12 -0400

    Added comment to play.h

--- src/play.h
+++ src/play.h
@@ -5,6 +5,7 @@
  *  Purpose: Make the header functions related to playing the card game UNO
  */
 
+ // Function headers
  void DrawCard(int numPlayers, Deck* player, Deck* deck, Deck* discard);
  void RebuildDeck(int numPlayers, Deck* deck, Deck* discard);
  int HumanTurn(Deck** playerList,  int numPlayers, Deck* playerHand, Deck* deck, Deck* discard, char* chosenColor);
